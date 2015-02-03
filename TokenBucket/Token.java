191f23e0f0416cbcecaedb3e73d1a08a39696d5c
Amrita <iyeramrita8@gmail.com>
2015-02-03 10:56:55 +0530

    Update Token.java

--- TokenBucket/Token.java
+++ TokenBucket/Token.java
@@ -1,20 +1,12 @@
 package TokenBucket;
 
-public class Token 
-{
-  private int count=0;
-  private int id=0;
-  Token()
-  {
-	  id=id+1;
-  }
-  public int get_id()
-  {
-	  return id++;
-  }
-  public int get_count()
-  {
-	return count++;
-  }
-  
-}
+
+	public class Token 
+	{
+	  private int token_id=0;
+	  public int get_id()
+	  {
+		 return token_id++;
+	  }
+	  
+	}
