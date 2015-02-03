fa64f8357eb2467b93a5bb2a44d89989187d7845
Amrita <iyeramrita8@gmail.com>
2015-02-03 10:41:46 +0530

    Update packet_thread.java

--- TokenBucket/packet_thread.java
+++ TokenBucket/packet_thread.java
@@ -20,11 +20,11 @@ public class packet_thread extends Thread
 	    	try 
 	    	{
 				sleep(1000);
-			}
+	        }
 	    	catch (InterruptedException e)
 	    	{
 				e.printStackTrace();
-			}
+		}
 	    	}
 	    }
 	  
