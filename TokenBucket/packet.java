c6ca12382128823eb5dca4552c0c214fd93be6d1
Amrita <iyeramrita8@gmail.com>
2015-02-03 10:55:30 +0530

    Update packet.java

--- TokenBucket/packet.java
+++ TokenBucket/packet.java
@@ -1,22 +1,13 @@
-package TokenBucket;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.*;
-//packet class
+
 public class packet 
-{   
-	private int id=0;
-	
-	private int packet_size=0;
-	private int count=0;	
-	Date created;
-    Date moved;
+{ 
+	private int id=0;	
+	Date created,moved;
 	private int tokens_required;
 	
 	packet()
 	{
 		created=new Date();
-		id=id+1;
 	}
 	public int getId()
 	{
@@ -43,3 +34,4 @@ public class packet
 		return moved;
 	}
 }
+
