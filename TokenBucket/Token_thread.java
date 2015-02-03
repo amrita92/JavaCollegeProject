8e6e56a1b9053cfee9bbc124804dd0c791bb5991
Amrita <iyeramrita8@gmail.com>
2015-02-03 10:51:40 +0530

    Update Token_thread.java

--- TokenBucket/Token_thread.java
+++ TokenBucket/Token_thread.java
@@ -1,37 +1,31 @@
 package TokenBucket;
 
+package project;
+
 public class Token_thread extends Thread
 {
+	
 	Token t=new Token();
-	Bucket b=new Bucket(20);
+	Bucket b;
 	Queue<Token> token_queue;
-
-	Token_thread(Queue<Token> token_queue)
+	Token_thread(Queue<Token> token_queue,Bucket b)
 	{
 		this.token_queue=token_queue;
+		this.b=b;
 	}
 	public void run()
  	{
 	 while(true)
-	 {
-		if(b.buc_size()>t.get_count())           
-		{		
-		  token_queue.enqueue(t);
-		  System.out.println("Token added:"+t.get_id());
+	 {	
+		  System.out.println("Token added:"+t.get_id());	
+		  b.add_tokens_to_bucket();	
 		  try
 		  {
-			sleep(2000);
+			Thread.sleep(500);
 		  } 
 		  catch (InterruptedException e)
 		  {
-			e.printStackTrace();
-		  }		 	  
-		}
-		else
-		{
-			System.out.println("The bucket is full.");
-		}
+			System.out.println("thread interrupted");
+		  }	
 	 }
-	 
-	}
-}
+	 }
