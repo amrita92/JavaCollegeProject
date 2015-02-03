7f989ae78a5d8e112daa24cd99b5faf42e11e173
Amrita <iyeramrita8@gmail.com>
2015-02-03 10:59:29 +0530

    Update Server_thread.java

--- TokenBucket/Server_thread.java
+++ TokenBucket/Server_thread.java
@@ -1,46 +1,52 @@
+package project;
 public class Server_thread extends Thread
 {
-  Queue<packet> que2;
-  packet p=new packet();
-  int i=0;
-  private final int MU=10000;
-  Server_thread(Queue<packet> que2)
-  {
-	this.que2=que2;
-  }
-  public void run() 
-  {
-	  if(que2.equals(null))
-      {
-	    try 
- 	    {
-    	 que2.wait();
-	    } 
-	    
-        catch (InterruptedException e)
-        {
-	     e.printStackTrace();
-	    }
-      }
-	  else
+ public class Server_thread extends Thread
+{
+	 Queue<packet> que2;
+	  packet p=new packet();
+	  int i=0;
+	  private final int MU=3000;
+	  Server_thread(Queue<packet> que2)
 	  {
-		  while(que2!=null)
+		this.que2=que2;
+	  }
+	  public void run() 
+	  {
+		  if(que2==null)
+	      {
+		    try 
+	 	    {
+	    	 que2.wait();
+		    } 
+		    
+	        catch (InterruptedException e)
+	        {
+		     e.printStackTrace();
+		    }
+	      }
+		  else
 		  {
+			  while(que2!=null)
+			  {
+				  
+			  System.out.println("Queue_two is getting dequeued:"+que2.dequeue());
+			  try 
+			  {
+				Thread.sleep(MU);
+			  } 
 			  
-		  System.out.println("Queue_two is getting dequeued:"+que2.dequeue());
-		  try 
-		  {
-			Thread.sleep(MU);
-		  } 
-		  
-		  catch (InterruptedException e)
-		  {
-			e.printStackTrace();
-		  }
-		  
+			  catch (InterruptedException e)
+			  {
+				e.printStackTrace();
+			  }
+			  
+			  }
 		  }
 	  }
-  }
+
+}
+
 }
 
 
